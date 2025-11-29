7e9812ca8f50a01bc5afcb31f7ba3214c1faf8cd
Le-soleile <3516093767@qq.com>
2025-11-29 13:04:02 +0000

    l1_norm_kernel需要修改paddle库

--- backends/metax_gpu/kernels/cuda_kernels/l1_norm_kernel_register.cu
+++ backends/metax_gpu/kernels/cuda_kernels/l1_norm_kernel_register.cu
@@ -13,6 +13,6 @@
 // limitations under the License.
 
 #include "paddle/phi/core/kernel_registry.h"
-#include "paddle/phi/kernels/l1_norm_kernel.h"
+#include "paddle/phi/kernels/gpu/l1_norm_kernel.cu"  // NOLINT
 PD_CUSTOM_KERNEL_REGISTER(
     l1_norm, metax_gpu, ALL_LAYOUT, phi::L1NormKernel, float) {}
