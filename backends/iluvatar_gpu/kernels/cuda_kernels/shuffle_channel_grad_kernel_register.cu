a43648aa634cc4b57a3c41a4250ae7dbc2c6966f
Le-soleile <3516093767@qq.com>
2025-12-02 14:06:30 +0000

    error path

--- backends/iluvatar_gpu/kernels/cuda_kernels/shuffle_channel_grad_kernel_register.cu
+++ backends/iluvatar_gpu/kernels/cuda_kernels/shuffle_channel_grad_kernel_register.cu
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 #include "paddle/phi/core/kernel_registry.h"
-#include "paddle/phi/kernels/gpu/shuffle_channel.h"
+#include "paddle/phi/kernels/shuffle_channel.h"
 #include "paddle/phi/kernels/shuffle_channel_grad_kernel.h"
 
 PD_CUSTOM_KERNEL_REGISTER(shuffle_channel_grad,
