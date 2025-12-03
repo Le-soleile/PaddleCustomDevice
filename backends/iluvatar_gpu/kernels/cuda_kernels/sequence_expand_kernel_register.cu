e344e48a5cff86d181366b1cbf4106ed05ba6e66
Le-soleile <3516093767@qq.com>
2025-12-03 15:13:57 +0000

    fix

--- backends/iluvatar_gpu/kernels/cuda_kernels/sequence_expand_kernel_register.cu
+++ backends/iluvatar_gpu/kernels/cuda_kernels/sequence_expand_kernel_register.cu
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "paddle/phi/kernels/gpu/sequence_expand_kernel.h"
 #include "paddle/phi/kernels/impl/sequence_expand_kernel_impl.h"
+#include "paddle/phi/kernels/sequence_expand_kernel.h"
 
 PD_CUSTOM_KERNEL_REGISTER(sequence_expand,
                           iluvatar_gpu,
