ce6080d8b0d90cd3e941b2257ee958adc3bb2a63
Yami <156195357+Le-soleile@users.noreply.github.com>
2025-12-14 20:14:37 +0800

    Fix include statement for fused_softmax_mask_upper_triangle

--- backends/iluvatar_gpu/kernels/cuda_kernels/fused_softmax_mask_upper_triangle_kernel_register.cu
+++ backends/iluvatar_gpu/kernels/cuda_kernels/fused_softmax_mask_upper_triangle_kernel_register.cu
@@ -13,7 +13,7 @@
 // limitations under the License.
 #include "paddle/phi/core/kernel_registry.h"
 #include "paddle/phi/kernels/fused_softmax_mask_upper_triangle_kernel.h"
-#include "paddle/phi/kernels/fusion/fused_softmax_mask_upper_triangle_kernel.h"
+#include "paddle/phi/kernels/fused_softmax_mask_upper_triangle_kernel.h"
 #include "paddle/phi/kernels/fusion/gpu/fused_softmax_mask_upper_triangle_utils.h"
 
 PD_CUSTOM_KERNEL_REGISTER(fused_softmax_mask_upper_triangle,
